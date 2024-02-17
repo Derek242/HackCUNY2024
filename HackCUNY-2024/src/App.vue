f33b3dca3d3cdca67f8ea372c517bcf78c6bf627
Derek <derek.ni47@myhunter.cuny.edu>
2024-02-16 23:54:26 -0500

    tweaked css

--- HackCUNY-2024/src/App.vue
+++ HackCUNY-2024/src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
  //import HelloWorld from './components/HelloWorld.vue'
+import { ref, toHandlerKey } from 'vue'
+import axios from 'axios'
 </script>
 
 <template>
@@ -17,16 +19,16 @@
         <header>
             <nav class="navbar">
               <!-- Left Side for the logo and name-->
-              <div>
-                <a href="../src/HelloWorld.vue" rel="Links to main page" class="Logo">
+              <div class="Logo">
+                <a href="../src/HelloWorld.vue" rel="Links to main page" >
                   <img src="../src/pictures/4fingers.png" alt="Logo">
                 </a>
               </div>
 
               <!-- Right side for login-->
-              <div>
-                <a href="../src/components/HelloWorld.vue" rel="Testing Link" class="Login">
-                  <img src="" alt="Login">
+              <div class="Login">
+                <a href="../src/components/HelloWorld.vue" rel="Testing Link">
+                  <img src="../src/pictures/keshi.jpg" alt="Login">
                 </a>
               </div>
             </nav>
@@ -47,7 +49,7 @@
 
         <input type="text" id="car-name" v-model=carmake @keyup.enter=searchbar @keyup=SearchChanged(carmake) placeholder = "Car Company">
         <div class = "cars-container">
-        <ul>cars:
+        <ul>cars: 
         <li v-for="(car,i) in cardata" :key="i">
           <p>
           model: {{ car.model }}<br/>
@@ -91,8 +93,6 @@ export default {
   },
 };
 
-import { ref, toHandlerKey } from 'vue'
-import axios from 'axios'
 
 const count = ref(0)
 let search = '';
@@ -122,45 +122,36 @@ function searchbar(){
 
 
 <style scoped>
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-} 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+/* #car-website {
+
 } */
 
-#car-website {
-  display: flex;
-}
 header {
   margin-bottom: 20px;
   display: block;
   justify-content: space-between;
   padding: 20px;
-  flex-direction: row;
+  display: flex;
 }
-.Logo {
-  display: flex; 
-  align-items: center;
+.Logo, .Login{
   
 }
 
-.Logo img {
-  max-width: 30px;
+.Logo img, .Login img {
+  max-width: 25%;
   height: auto;
-  margin-right: 30px;
+  margin-right: 25%;
 }
+
 
 .navbar {
   background-color: #333;
   color: white;
   padding: 10px 20px;
+  display: flex;
+  flex-direction: column;
+
 }
 
 .navbar ul {
