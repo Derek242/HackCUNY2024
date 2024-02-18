2c92d2ecb3b8ebcdacb1610664cbb40ec6588858
EvannLu <evan.l60@myhunter.cuny.edu>
2024-02-17 23:25:42 -0500

    update

--- HackCUNY-2024/src/components/TopBar.vue
+++ HackCUNY-2024/src/components/TopBar.vue
@@ -22,11 +22,13 @@
 
 .topBar {
     background-color: gray;
+    border-bottom: solid;
     padding: 2%;
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
     bottom: 620px;
+    z-index: 200;
 }
 </style>
